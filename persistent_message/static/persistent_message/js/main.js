962520b23bf223755548f8a3cb588ede9810fcda
Jim Laney <jlaney@uw.edu>
2018-12-21 14:11:02 -0800

    implement post/put/delete on client

--- persistent_message/static/persistent_message/js/main.js
+++ persistent_message/static/persistent_message/js/main.js
@@ -10,7 +10,11 @@
     }
 
     function format_date(date_str) {
-        return moment(date_str).format("MMMM D[,] YYYY [at] h:mm A");
+        var dt = moment(date_str);
+        if (dt.isValid()) {
+            return dt.format('MMMM D[,] YYYY [at] h:mm A');
+        }
+        return '';
     }
 
     Handlebars.registerHelper('format_date', function(date_str) {
@@ -19,6 +23,15 @@
 
     Handlebars.registerHelper('if_equals', function(arg1, arg2, options) {
         return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+    });
+
+    Handlebars.registerHelper('has_tag', function(tags, name, options) {
+        for (var i = 0; i < tags.length; i++)  {
+            if (name === tags[i].name) {
+                return options.fn(this);
+            }
+        }
+        return options.inverse(this);
     });
 
     Handlebars.registerHelper('lower', function(str) {
@@ -52,9 +65,9 @@
             }).fail().done(cache_tag_groups).done(callback);
         }
 
-        function get_message() {
+        function get_message(message_id) {
             return $.ajax({
-                url: window.persistent_message.message_api,
+                url: window.persistent_message.message_api + '/' + message_id,
                 dataType: 'json'
             });
         }
@@ -78,48 +91,81 @@
 
         function delete_message() {
             /*jshint validthis: true */
-            var message_id = $(this).attr('id').replace('message-', '');
+            var message_id = $(this).attr('id').replace('pm-delete-', '');
 
             if (message_id.match(/^[0-9]+$/)) {
                 if (confirm('Delete this message?')) {
                     $.ajax({
-                        url: window.persistent_messsage.message_api + '/' + message_id,
+                        url: window.persistent_message.message_api + '/' + message_id,
                         dataType: 'json',
                         type: 'DELETE'
-                    }).fail(delete_error).done(load_messages);
+                    }).fail(delete_error).done(init_messages);
                 }
             }
         }
 
-        function add_message() {
-            var content = $.trim($("textarea[name='content']").val());
+        function _serialize_form() {
+            /*jshint loopfunc: true */
+            var data = {},
+                begins = $.trim($('input[name="begins"]').val()),
+                expires = $.trim($('input[name="expires"]').val()),
+                tag_groups_len = window.persistent_message.tag_groups.length,
+                group_tags,
+                group_name,
+                i;
 
+            data.content = $.trim($('textarea[name="content"]').val());
+            data.level = $('input[name="level"]:checked').val();
+            data.tags = [];
+            data.begins = null;
+            data.expires = null;
+
+            for (i = 0; i < tag_groups_len; i++)  {
+                group_name = window.persistent_message.tag_groups[i].name.toLowerCase();
+                group_tags = $('input[name="tag_' + group_name + '"]:checked').map(
+                    function () { return $(this).val(); }).get();
+                data.tags.push.apply(data.tags, group_tags);
+            }
+
+            if (begins !== null && begins.length) {
+                begins = moment(begins, 'L LT');
+                if (begins.isValid()) {
+                    data.begins = begins.utc().toISOString();
+                }
+            }
+
+            if (expires !== null && expires.length) {
+                expires = moment(expires, 'L LT');
+                if (expires.isValid()) {
+                    data.expires = expires.utc().toISOString();
+                }
+            }
+
+            return JSON.stringify({'message': data});
+        }
+
+        function add_message() {
+            var post_data = _serialize_form();
             $.ajax({
                 url: window.persistent_message.message_api,
                 dataType: 'json',
                 contentType: 'application/json',
                 type: 'POST',
-                data: JSON.stringify({content: content})
-            }).fail(load_form).done(load_confirmation);
+                data: post_data
+            }).fail(load_form).done(init_messages);
         }
 
         function update_message() {
-            var description = $.trim($("textarea[name='description']").val()),
-                name = $.trim($("input[name='name']").val());
+            var message_id = $('input[name="message_id"]').val(),
+                put_data = _serialize_form();
 
             $.ajax({
-                url: window.persistent_message.message_api,
+                url: window.persistent_message.message_api + '/' + message_id,
                 dataType: 'json',
                 contentType: 'application/json',
                 type: 'PUT',
-                data: JSON.stringify({name: name, description: description})
-            }).fail(load_form).done(init_form);
-        }
-
-        function load_confirmation(data) {
-            var template = Handlebars.compile($('#confirmation-tmpl').html());
-            $('#pm-content').html(template(data));
-            $('#pm-header').html(data.name);
+                data: put_data
+            }).fail(load_form).done(init_messages);
         }
 
         function load_form(data) {
@@ -127,8 +173,18 @@
             data.tag_groups = window.persistent_message.tag_groups;
             data.message_levels = window.persistent_message.message_levels;
             $('#pm-content').html(template(data));
-            //$('#pm-header').html(data.name);
-            $('button.pm-btn-submit').click(add_message);
+
+            $('#pm-begins-datetimepicker').datetimepicker({
+                locale: 'en',
+                format: 'L LT',
+                defaultDate: (data.message.begins) ? moment(data.message.begins) : false
+            });
+            $('#pm-expires-datetimepicker').datetimepicker({
+                locale: 'en',
+                format: 'L LT',
+                defaultDate: (data.message.expires) ? moment(data.message.expires) : false
+            });
+            $('button.pm-btn-submit').click((data.message.id) ? update_message : add_message);
             $('#pm-message-content').focus();
         }
 
@@ -136,9 +192,8 @@
             var template = Handlebars.compile($('#message-list-tmpl').html());
             data.tag_groups = window.persistent_message.tag_groups;
             $('#pm-content').html(template(data));
-            $('#pm-header').html('Messages');
-            //$('a.pm-btn-publish').click(publish_message);
-            $('a.pm-btn-delete').click(delete_message);
+            $('button.pm-btn-edit').click(init_edit_message);
+            $('button.pm-btn-delete').click(delete_message);
         }
 
         function load_error(xhr) {
@@ -160,8 +215,15 @@
             get_messages().fail(load_error).done(load_messages);
         }
 
+        function init_edit_message() {
+            /*jshint validthis: true */
+            var message_id = $(this).attr('id').replace('pm-edit-', '');
+            $('#pm-message-add-link').tab('show');
+            get_message(message_id).fail(load_error).done(load_form);
+        }
+
         function init_add_message() {
-            var data = {'message': {}};
+            var data = {'message': {level: 20, tags: []}};
             $('#pm-message-add-link').tab('show');
             load_form(data);
         }
