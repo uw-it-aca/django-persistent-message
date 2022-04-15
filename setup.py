a70fcacdc41617e54e51157a065d80f00148262d
Jim Laney <jlaney@uw.edu>
2022-04-15 14:22:52 -0700

    supports django3.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>=2.1,<3.2',
+        'Django>=2.1,<4.0',
         'python-dateutil',
         'bleach>=3.2.0',
     ],
@@ -38,6 +38,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
     ],
 )
