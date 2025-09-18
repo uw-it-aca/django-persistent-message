c64d1cc935cec4adefd5f4c0265bb8bfc0dde4a6
Fang Lin <fanglin@uw.edu>
2025-09-18 15:39:07 -0700

    remove django version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>=4.2,<6',
+        'Django',
         'python-dateutil',
         'nh3',
     ],
